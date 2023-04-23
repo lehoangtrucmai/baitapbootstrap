660a86d8af81e1b7f1e31121c8d520f1033a6497
LÊ HOÀNG TRÚC MAI <125021302+lehoangtrucmai@users.noreply.github.com>
2023-04-23 13:06:02 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Card from './component/card';
+import Bootstap from './component/header';
+
+function App() {
+  return (
+    <div >
+      <Card />
+      <Bootstap />
+      <footer />
+ 
+    </div>
+  );
+}
+
+export default App;
